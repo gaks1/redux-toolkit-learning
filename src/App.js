67807356b852e6d477e42ebba313d21777a5178c
isaacg <iwanyoike65@gmail.com>
2023-04-04 12:06:03 +0300

    Add calculate total and modal setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import Navbar from "./components/Navbar";
-import CartContainer from "./components/CartContainer";
+import { useEffect } from 'react';
+import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
+import { useSelector, useDispatch } from 'react-redux';
+import { calculateTotals } from './features/cart/cartSlice';
+import Modal from './components/Modal';
+
 function App() {
-  return(
+  const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
+  return (
     <main>
+      <Modal />
       <Navbar />
       <CartContainer />
     </main>
-  )
+  );
 }
+
 export default App;
